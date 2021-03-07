8916335e68210d14d7414a725790b8805e3f217e
Maadhav Sharma <maadhav2001@gmail.com>
2021-03-07 11:25:39 +0530

    model null checks for labels

--- lib/services/html_extract_service.dart
+++ lib/services/html_extract_service.dart
@@ -3,8 +3,13 @@ import 'package:html/parser.dart';
 class HtmlService {
   static String extractImage(String src) {
     var document = parse(src);
-    return document.getElementsByTagName('img').first.attributes['src'] ??
-        'https://cdn.shopify.com/s/files/1/1856/9187/articles/b2.jpg';
+    var url;
+    try {
+      url = document.getElementsByTagName('img')?.first?.attributes['src'];
+    } on StateError {
+      url = 'https://cdn.shopify.com/s/files/1/1856/9187/articles/b2.jpg';
+    }
+    return url;
   }
 
   static String extractText(String src) {
